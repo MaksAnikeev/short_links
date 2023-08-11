44ef02572515c204766caeb35ae1b90fa93e7577
MaksAnikeev <anikeev.maks@rambler.ru>
2023-08-11 07:29:16 +0300

    Tuning forms

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 class UserForm(forms.Form):
-    full_link = forms.CharField(max_length=500)
-    short_link = forms.CharField(max_length=100, required=False)
+    full_link = forms.CharField(max_length=500,
+                                label='полная ссылка',
+                                widget=forms.Textarea)
+    short_link = forms.CharField(max_length=100,
+                                 required=False,
+                                 label='короткая ссылка'
+                                 )
